16453d60d5d3ed07189502d642be27b58b866a95
Kiran Kumar SY <kiran_kumar.s_y@nokia.com>
2019-05-19 11:22:17 +0530

    SecondUser17thCheckout

--- PythonTest.py
+++ PythonTest.py
@@ -3,3 +3,4 @@ print("1st change from FirstUser");
 print("1st change from SecondUser");
 print("3rd change from FirstUser");
 print("4th change from FirstUser");
+print("2nd change from SecondUser");
