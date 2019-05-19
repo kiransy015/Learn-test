a03a2e88dd73f9e30589e304091bf106d86ff05c
Kiran Kumar SY <kiran_kumar.s_y@nokia.com>
2019-05-19 07:41:41 +0530

    SecondUser1stCheckout

--- JavaTest.java
+++ JavaTest.java
@@ -1,3 +1,4 @@
 System.out.println("Welcome to Java Project");
 System.out.println("2nd change from FirstUser");
 System.out.println("3rd change from FirstUser");
+System.out.println("1st change from SecondUser");
